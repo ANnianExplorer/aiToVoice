4de8461f952f520f345c14210c890c75b6e9aa12
ANnian2022 <3480088617@qq.com>
2026-06-11 12:50:43 +0800

    feat: init Spring Boot backend with pom.xml and application config

---
+++ backend/src/main/java/com/aitovoice/AitoVoiceApplication.java
@@ -0,0 +1,14 @@
+package com.aitovoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class AitoVoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AitoVoiceApplication.class, args);
+    }
+}
